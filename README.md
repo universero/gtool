cad43f0e5a9765e269a8f6501c3f09579a7ecb5f
univero <1449610641@qq.com>
2025-05-22 10:58:00 +0800

    feat: README

---
+++ README.md
@@ -0,0 +1,8 @@
+# gtool 🔧
+[![Go Report Card](https://goreportcard.com/badge/github.com/universero/gtool)](https://goreportcard.com/report/github.com/universero/gtool)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+A command-line tool built with Cobra, 
+collecting my frequently used development commands. 
+Continuously optimized.
+⭐ Stars are welcome!
--- cmd/root.go
+++ cmd/root.go
@@ -13,13 +13,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gtool",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "some useful command line for univero's dev (improving)",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
